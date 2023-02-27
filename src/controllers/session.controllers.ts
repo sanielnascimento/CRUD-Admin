3233801a95a7b07e8f0420273a33250c72b870db
Saniel1990 <saniel_sabr@outlook.com>
2023-02-27 09:31:17 -0300

    fix code indentation

--- src/controllers/session.controllers.ts
+++ src/controllers/session.controllers.ts
@@ -1,8 +1,5 @@
 import { Request, Response } from "express";
 import { loginSessionService } from "../services";
 
-export const loginSessionController = async (
-  req: Request,
-  res: Response
-): Promise<Response> =>
+export const loginSessionController = async (req: Request, res: Response): Promise<Response> =>
   res.status(201).json(await loginSessionService(req.body));
--- src/services/users/recoverUsers.service.ts
+++ src/services/users/recoverUsers.service.ts
@@ -6,16 +6,16 @@ import { userSchema } from "../../schemas";
 export const recoverUserService = async (
   id: number
 ): Promise<tUserResponse> => {
-  let queryResult: UserResult = await client.query(
+  const querySelect: UserResult = await client.query(
     "SELECT * FROM users WHERE id = $1;",
     [id]
   );
 
-  if (queryResult.rows[0].active === true)
+  if (querySelect.rows[0].active === true)
     throw new AppError("User already active", 400);
 
-  queryResult = await client.query(
-    "UPDATE users SET active = true WHERE id = $1;",
+  const queryResult: UserResult = await client.query(
+    "UPDATE users SET active = true WHERE id = $1 RETURNING *;",
     [id]
   );
 
